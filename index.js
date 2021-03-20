8c94964d791e525f3a286e7b7357175a07c92600
Sebastian Diaz <sebastian.diazg@outlook.com>
2021-03-20 13:52:53 -0300

    Se inizializa el proyecto

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+require('dotenv').config();
+
+const app = express();
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+    res.json({
+        consulta: 'ok'
+    })
+})
+
+app.listen(process.env.PORT, () => { console.log(`Servidor corriendo en el puerto ${process.env.PORT}`) })
