f1df7836e22c08bcf11634774889ec87767458ec
Sebastian Diaz <sebastian.diazg@outlook.com>
2021-03-20 14:07:53 -0300

    Se agrega body parser y cors

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
+'use strict';
+
 const express = require('express');
+const cors = require('cors')
+const bodyParser = require('body-parser');
+
 require('dotenv').config();
 
 const app = express();
+
+app.use(cors())
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 app.use(express.static('public'))
 
